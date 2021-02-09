9f72653f730937b51885b00fc449486d099aa308
DusanCvetkovic <71986600+DusanCvetkovic@users.noreply.github.com>
2021-02-09 21:49:05 +0100

    Add files via upload

---
+++ Script2.js
@@ -0,0 +1,19 @@
+const name = 'Dusan';
+const age = 24;
+
+const hello = `My name is ${name} and I am ${age}`;
+
+console.log(hello);
+
+
+const s = 'Hello, technology, code';
+
+console.log(s.length);
+
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+
+console.log(s.substring(0, 5).toUpperCase());
+console.log(s.substring(6, 11).toLowerCase());
+
+console.log(s.split(', '));
